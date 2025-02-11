6797bda6d3e23ed1e867f09df2fb051950ea3250
maiktreya <miguel.garcia.duch@gmail.com>
2025-02-11 12:12:02 +0100

    Update rentaINE calculation in reproduce.R to include IPC adjustment

--- ECV/reproduce.R
+++ ECV/reproduce.R
@@ -41,7 +41,7 @@ quantiles <- data.table(tramo = quantile_cuts, renta_nom = quantiles_renta, rent
 ratio <- quantiles[tramo == "0.9", renta_real] / quantiles[tramo == "0.5", renta_real] %>% round(3)
 
 # Comprobar ine
-rentaINE <- svymean(~vhRentaa, design = survey_total, na.rm = TRUE)
+rentaINE <- svymean(~vhRentaa, design = survey_total, na.rm = TRUE)[1] / ipc[AÑO_RENTA == as.numeric(paste0(20, sel_year))]$deflactor_IPC
 
 # Show results
 print(rentaINE) # deber ser 32216 https://www.ine.es/jaxiT3/Datos.htm?t=9948
